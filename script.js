ca46a28ed94973efe2fdb5403fcde3886c0ff4a9
austin109lew <150309175+austin109lew@users.noreply.github.com>
2024-01-11 19:49:12 -0500

    Combined Income/Expense Tracker to main branch

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-  fetch("https://type.fit/api/quotes")
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      const quotes = data;
-      const currentDate = new Date();
-      const randomIndex = currentDate.getDate() % quotes.length;
-      const quoteContainer = document.getElementById("quote-container");
-      const quoteText = document.getElementById("quote-text");
-  
-      quoteText.textContent = quotes[randomIndex].text;
-      quoteContainer.style.display = "block";
-    })
-    .catch(error => console.error("Error fetching quotes:", error));
-  });
-
+    fetch("https://type.fit/api/quotes")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        const quotes = data;
+        const currentDate = new Date();
+        const randomIndex = currentDate.getDate() % quotes.length;
+        const quoteContainer = document.getElementById("quote-container");
+        const quoteText = document.getElementById("quote-text");
+    
+        quoteText.textContent = quotes[randomIndex].text;
+        quoteContainer.style.display = "block";
+      })
+      .catch(error => console.error("Error fetching quotes:", error));
+    });
   
